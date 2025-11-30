5d091240e2b6539d0ef656f396e9679a01895645
Komakulakavya <125781517+Komakulakavya@users.noreply.github.com>
2025-11-30 20:01:17 +0530

    Update Flask app response message

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(__name__)
-@app.route("/")
+
+app = Flask(__name__)
+
+@app.route('/')
 def home():
-  return "hi from flask"
-if __name__=="__main__":
-  app.run(host='0.0.0.0',port=5000)
+    return "Hello from Dockerized Flask App!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
